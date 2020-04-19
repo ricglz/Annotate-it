cbddd3fed12fb2ab8036e7ebe36aa540be224a84
Ricardo González Castillo <rjgcastillo@hotmail.com>
2020-04-19 18:51:31 -0500

    Update reek and config related things

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationCable
+  # General connection for ApplicationCable
   class Connection < ActionCable::Connection::Base
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,6 @@ require "action_cable/engine"
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module ProjectBackend
@@ -24,14 +22,14 @@ module ProjectBackend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.test_framework  false
+      g.stylesheets     false
+      g.javascripts     false
+      g.helper          false
+      g.channel         assets: false
+    end
   end
 end
