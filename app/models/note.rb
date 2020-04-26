3a29e85762873c12f0b9687d2867f2721e1c3e80
Ricardo J. González C <rjgcastillo@hotmail.com>
2020-04-26 13:38:46 -0500

    Run migration of db

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,8 +3,8 @@
 class Note < ApplicationRecord
   acts_as_paranoid
 
+  belongs_to :folder, dependent: :delete, inverse_of: :notes, optional: true
   belongs_to :user, inverse_of: :notes
   has_many :notes_tags, dependent: :delete, inverse_of: :notes
   has_many :tags, through: :notes_tags, dependent: :delete, inverse_of: :notes
-  belongs_to :folder, dependent: :delete, inverse_of: :notes, optional: true
 end
---
+++ db/schema.rb
@@ -0,0 +1,68 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_04_20_001151) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "folders", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_folders_on_user_id"
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.bigint "folder_id"
+    t.datetime "deleted_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["folder_id"], name: "index_notes_on_folder_id"
+    t.index ["user_id"], name: "index_notes_on_user_id"
+  end
+
+  create_table "notes_tags", force: :cascade do |t|
+    t.bigint "note_id", null: false
+    t.bigint "tag_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["note_id"], name: "index_notes_tags_on_note_id"
+    t.index ["tag_id"], name: "index_notes_tags_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tags_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "folders", "users"
+  add_foreign_key "notes", "folders"
+  add_foreign_key "notes", "users"
+  add_foreign_key "notes_tags", "notes"
+  add_foreign_key "notes_tags", "tags"
+  add_foreign_key "tags", "users"
+end
