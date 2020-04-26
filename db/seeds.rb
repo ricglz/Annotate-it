87596671c6d3a04cd455ed6d30c9e71d5199b1f1
Ricardo J. González C <rjgcastillo@hotmail.com>
2020-04-26 14:16:39 -0500

    Create seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating users'
+users = FactoryBot.create_list :user, 5
+users.each_with_index do |user|
+  puts "Creating user-#{user.id}"
+  folders = FactoryBot.create_list :folder, 3, user: user
+  tags = FactoryBot.create_list :tag, 3, user: user
+  folders.each do |folder|
+    notes_amount = (rand() * 4).to_i + 1
+    notes = FactoryBot.create_list :note, notes_amount,
+                                   user: user, folder: folder
+    notes.each do |note|
+      tags_amount = (rand() * 2).to_i + 1
+      note.tags = tags.sample(tags_amount)
+    end
+  end
+end
