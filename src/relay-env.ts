4633c15cf6ffe057e73334f120446eac960de7d4
Ricardo J. González C <rjgcastillo@hotmail.com>
2020-05-03 11:36:00 -0500

    Add relay dependency

---
+++ src/relay-env.ts
@@ -0,0 +1,35 @@
+import {
+  Environment,
+  Network,
+  RecordSource,
+  RequestParameters,
+  Store,
+  Variables
+} from 'relay-runtime';
+
+const url = 'localhost:3000'
+
+function fetchQuery(
+  operation: RequestParameters,
+  variables: Variables,
+) {
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: operation.text,
+      variables,
+    }),
+  }).then(response => {
+    return response.json();
+  });
+}
+
+const environment = new Environment({
+  network: Network.create(fetchQuery),
+  store: new Store(new RecordSource()),
+});
+
+export default environment;
