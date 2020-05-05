954cb59eb32d7464ea67ae8d5e0f6f4e998f9a55
Ricardo J. González C <rjgcastillo@hotmail.com>
2020-05-04 20:17:57 -0500

    Remove warden file, modify cors and fix base_mutation

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'localhost:3000'
-    resource '*', headers: :any, methods: %i[post], credentials: true
+    resource '*', headers: :any, methods: %i[post]
   end
 end
