5042cbbf03ad2d0dfe7b9aff8172daf92b875ccb
Ricardo J. González C <rjgcastillo@hotmail.com>
2020-05-12 15:04:15 -0500

    Add to look for an specific folder and note in user

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -8,11 +8,29 @@ module Types
     field :name, String, null: true
     field :email, String, null: true
 
+    field :note, Types::NoteType,
+          null: true, description: 'An Specific note' do
+      argument :id, ID, required: true
+    end
     field :notes, Types::NoteType.connection_type,
           null: false, description: 'Notes made by the user'
+
+    field :folder, Types::FolderType,
+          null: true, description: 'An Specific folder' do
+      argument :id, ID, required: true
+    end
     field :folders, Types::FolderType.connection_type,
           null: false, description: 'Folders made by the user'
+
     field :tags, Types::TagType.connection_type,
           null: false, description: 'Tags made by the user'
+
+    def note(input)
+      object.notes.find(input[:id])
+    end
+
+    def folder(input)
+      object.folders.find(input[:id])
+    end
   end
 end
