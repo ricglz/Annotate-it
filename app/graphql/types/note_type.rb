8153c23d8e753b5bf8eaf9a1c38a71bc4100f6e6
Ricardo J. González C <rjgcastillo@hotmail.com>
2020-05-14 13:44:21 -0500

    Add title field for note

--- app/graphql/types/note_type.rb
+++ app/graphql/types/note_type.rb
@@ -4,11 +4,16 @@ module Types
   class NoteType < Types::BaseObject
     description 'A note'
 
+    field :title, String, null: true
     field :content, String, null: true
     field :id, ID, null: false
     field :folder, Types::FolderType, null: true
     field :tags, Types::TagType.connection_type,
           null: false, description: 'Tags that are representing this note'
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    def title
+      object.content[0..50]
+    end
   end
 end
