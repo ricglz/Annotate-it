985d0c8994281fec3eef618d50a79b26a7bd96a5
Ricardo J. González C <rjgcastillo@hotmail.com>
2020-05-16 13:23:06 -0500

    Can create new notes belonging to a folder

--- app/graphql/mutations/create_note.rb
+++ app/graphql/mutations/create_note.rb
@@ -5,15 +5,16 @@ module Mutations
     field :edge, Types::NoteType.edge_type, null: false
 
     argument :email, String, required: true
-    argument :name, String, required: true
-    argument :note_id, ID, required: true
+    argument :content, String, required: true
+    argument :folder_id, ID, required: true
 
     def resolve(arguments)
       user = User.find_by(email: arguments[:email])
       raise GraphQL::ExecutionError, "This user doesn't exist" unless user
 
       note = Note.new(
-        name: arguments[:name], user: user, folder_id: arguments[:folder_id]
+        content: arguments[:content], user: user,
+        folder_id: arguments[:folder_id]
       )
       unless note.save
         raise GraphQL::ExecutionError, "This note doesn't belong to the user"
