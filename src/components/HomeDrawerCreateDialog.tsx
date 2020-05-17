d70993e0590a70c2d16d7b0a6ba1c43070b22c38
Ricardo J. González C <rjgcastillo@hotmail.com>
2020-05-17 10:15:18 -0500

    Create reusable component for the form of the dialog of folders

--- src/components/HomeDrawerCreateDialog.tsx
+++ src/components/HomeDrawerCreateDialog.tsx
@@ -1,58 +1,28 @@
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import HomeDrawerDialogForm from './HomeDrawerDialogForm';
 import React from 'react';
-import TextField from '@material-ui/core/TextField';
-import createStyles from '@material-ui/core/styles/createStyles';
-import makeStyles from '@material-ui/core/styles/makeStyles';
 import useCreateFolderMutation from '../mutations/useCreateFolderMutation';
-import { Theme } from '@material-ui/core/styles/createMuiTheme';
 
 interface Props {
   open: boolean;
   toogle: (e: any) => void;
 }
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    actions: {
-      justifyContent: 'space-between'
-    },
-  }),
-);
-
 const HomeDrawerCreateDialog = ({ open, toogle }: Props) => {
-  const { actions } = useStyles();
   const [
     onClick, { loading, name, onChange }
   ] = useCreateFolderMutation(toogle);
   return (
-    <Dialog
-      aria-labelledby="create-folder-dialog"
-      onClose={toogle}
+    <HomeDrawerDialogForm
+      identifier="create-folder-dialog"
+      label="Create"
+      loading={loading}
+      loadingLabel="Creating..."
+      name={name}
+      onChange={onChange}
+      onClick={onClick}
       open={open}
-    >
-      <DialogTitle id="create-folder-dialog">Create</DialogTitle>
-      <DialogContent>
-        <TextField
-          autoFocus
-          fullWidth
-          id="name"
-          label="Name of folder"
-          onChange={onChange}
-          margin="dense"
-          value={name}
-        />
-      </DialogContent>
-      <DialogActions className={actions}>
-        <Button color="secondary" onClick={toogle}>Cancel</Button>
-        <Button color="primary" onClick={onClick}>
-          { loading ? 'Creating' : 'Create' }
-        </Button>
-      </DialogActions>
-    </Dialog>
+      toogle={toogle}
+    />
   )
 }
 
---
+++ src/components/HomeDrawerDialogForm.tsx
@@ -0,0 +1,62 @@
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import createStyles from '@material-ui/core/styles/createStyles';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import { Theme } from '@material-ui/core/styles/createMuiTheme';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    actions: {
+      justifyContent: 'space-between'
+    },
+  }),
+);
+
+interface Props {
+  identifier: string;
+  label: string;
+  loading: boolean;
+  loadingLabel: string;
+  name: string;
+  onChange: (e: any) => void;
+  onClick: (e: any) => void;
+  open: boolean;
+  toogle: (e: any) => void;
+}
+
+const HomeDrawerDialogForm = ({
+  identifier, label, loading, loadingLabel, name, onChange, onClick,
+  open, toogle
+}: Props) => {
+  const { actions } = useStyles();
+  return (
+    <Dialog aria-labelledby={identifier} onClose={toogle} open={open}>
+      <DialogTitle id={identifier}>{label}</DialogTitle>
+      <DialogContent>
+        <TextField
+          autoFocus
+          fullWidth
+          id="name"
+          label="Name of folder"
+          onChange={onChange}
+          margin="dense"
+          value={name}
+        />
+      </DialogContent>
+      <DialogActions className={actions}>
+        <Button color="secondary" onClick={toogle}>Cancel</Button>
+        <Button color="primary" onClick={onClick}>
+          { loading ? loadingLabel : label }
+        </Button>
+      </DialogActions>
+    </Dialog>
+
+  );
+};
+
+export default HomeDrawerDialogForm;
