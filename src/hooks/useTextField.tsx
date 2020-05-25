13e9f69d0192604b1ca876e553394566b9ee145e
Ricardo J. González C <rjgcastillo@hotmail.com>
2020-05-24 21:19:16 -0500

    Implement search of notes

--- src/hooks/useTextField.tsx
+++ src/hooks/useTextField.tsx
@@ -1,10 +1,13 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, ChangeEvent } from 'react';
 
-type useTextFieldCallback = [string, (e: React.ChangeEvent<HTMLTextAreaElement>) => void];
+type useTextFieldCallback = [
+  string,
+  (e: ChangeEvent<HTMLTextAreaElement>) => void,
+];
 
-function useTextField(initialValue: string): useTextFieldCallback {
+function useTextField(initialValue: string = ''): useTextFieldCallback {
   const [text, setText] = useState(initialValue);
-  const onChange = useCallback((e: React.ChangeEvent<HTMLTextAreaElement>) => {
+  const onChange = useCallback((e: ChangeEvent<HTMLTextAreaElement>) => {
     setText(e.target.value);
   }, []);
   return [text, onChange];
