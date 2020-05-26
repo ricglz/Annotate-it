328fc3de636c05d817b922b570a2dc8e8bfcaa3f
Ricardo J. González C <rjgcastillo@hotmail.com>
2020-05-26 18:25:05 -0500

    Add react-alert, still lacks implementation

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,8 +1,15 @@
+import AlertTemplate from './AlertTemplate';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import React, { ReactNode } from 'react'
 import environment from '../config/relay-env';
 import { BrowserRouter as Router } from 'react-router-dom'
 import { ChangeThemeProvider } from '../contexts/ChangeThemeContext';
+import {
+  AlertProviderProps,
+  Provider as AlertProvider,
+  positions,
+  transitions,
+} from 'react-alert';
 import { RelayEnvironmentProvider } from 'relay-hooks';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { UserProvider } from '../contexts/UserContext';
@@ -13,6 +20,15 @@ interface Props {
   children: ReactNode
 }
 
+const alertProviderProps: AlertProviderProps = {
+  offset: '30px',
+  position: positions.BOTTOM_RIGHT,
+  template: AlertTemplate,
+  timeout: 3000,
+  transition: transitions.FADE,
+  type: 'error',
+}
+
 function Providers({ children }: Props) {
   const [theme, changeTheme] = useTheme();
   const currentState = useUser();
@@ -20,13 +36,15 @@ function Providers({ children }: Props) {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <ChangeThemeProvider value={changeTheme}>
-        <RelayEnvironmentProvider environment={environment}>
-          <UserProvider value={currentState}>
-            <Router>
-              {children}
-            </Router>
-          </UserProvider>
-        </RelayEnvironmentProvider>
+        <AlertProvider {...alertProviderProps}>
+          <RelayEnvironmentProvider environment={environment}>
+            <UserProvider value={currentState}>
+              <Router>
+                {children}
+              </Router>
+            </UserProvider>
+          </RelayEnvironmentProvider>
+        </AlertProvider>
       </ChangeThemeProvider>
     </ThemeProvider>
   )
